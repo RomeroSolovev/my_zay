0bbf2c7503e95766a77845d51ee7e83f31ddea95
Konsta2 <90917008+Konsta2@users.noreply.github.com>
2021-11-22 00:38:08 +0300

    Delete 2.js

--- 5/2.js
+++
@@ -1,9 +0,0 @@
-function  ymn(){
-    var number1,number2,res;
-    number1= document.getElementById('a1').value;
-    number1=parseInt(number1);
-    number2= document.getElementById('a2').value;
-    number2=parseInt(number2);
-    res=number1*number2;
-    document.getElementById('result').innerHTML=res;
-}
